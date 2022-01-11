0d93a135c71a2a3d32b27a8f1dbe37a1267a62e1
Vishnuraj Saravanan <76027763+vishnu02-droid@users.noreply.github.com>
2022-01-11 11:37:04 +0530

    Create member.js

---
+++ member.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: "Jason Derulo",
+        email: "jasonderulo2hiphop@onclick.com",
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: "Bob Wilson",
+        email: "bobwilson@onclick.com",
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: "Jack Bucky",
+        email: "jackbucky45@onclick.com",
+        status: 'active'
+    },
+];
+
+module.exports = members;
